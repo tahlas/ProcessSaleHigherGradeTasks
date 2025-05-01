c44e63fa1f88b3b13739b77c8a14f5a99ee87907
tahmid <tahmidlasker@gmail.com>
2025-05-01 09:34:47 +0200

    Moved all methods (except pay for items) to Sale.

--- src/model/Item.java
+++ src/model/Item.java
@@ -19,50 +19,7 @@ public class Item {
         payment.calculateTotalCost(soldItems);//1.3.1
     }
 
-    /**
-     * Adds a scanned item to the list that holds the items being sold.
-     * @param scannedItem The item to add to the list.
-     */
-    public void addScannedItemToList(ItemDTO scannedItem){
-        soldItems.add(scannedItem);
-    }
 
-    /**
-     * Gets the list with the sold items.
-     * @return ArrayList with sold items.
-     */
-    public ArrayList<ItemDTO> getSoldItems(){
-        return soldItems;
-    }
-
-    public int getQuantity(String specificItemID){
-        int numberOfSpecificItem = 0;
-        String currentItemInListID;
-        for(ItemDTO item : soldItems){
-            currentItemInListID = item.getID();
-            if(itemIDIsEqual(currentItemInListID, specificItemID)){
-                numberOfSpecificItem++;
-            }
-        }
-        return numberOfSpecificItem;
-    }
-
-    /**
-     * Checks if two item IDs are equal.
-     * @param firstItemID The first item ID to compare
-     * @param secondItemID THe second item ID to compare
-     * @return True if they are equal, false if they are not.
-     */
-    private boolean itemIDIsEqual(String firstItemID, String secondItemID){
-        return firstItemID.equals(secondItemID);
-    }
-
-
-
-    /**
-     * Prints the receipt.
-     * @param receipt The receipt to print.
-     */
 
 
 
