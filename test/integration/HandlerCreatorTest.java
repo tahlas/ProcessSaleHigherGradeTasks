241111455b6a276adbfdfda7555abd802bdde097
tahmid <tahmidlasker@gmail.com>
2025-05-02 11:51:46 +0200

    Tests

--- test/integration/HandlerCreatorTest.java
+++ test/integration/HandlerCreatorTest.java
@@ -8,9 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HandlerCreatorTest {
 
+    private HandlerCreator handlerCreator;
+
+    @BeforeEach
+    void setUp() {
+        handlerCreator = new HandlerCreator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        handlerCreator = null;
+    }
+
     @Test
-    void getInventoryHandler() {
+    void testGetInventoryHandlerNotNull() {
+        assertNotNull(handlerCreator.getInventoryHandler(), "The inventory handler should not be null");
+    }
 
-
+    @Test
+    void getInventoryHandlerSameInstance() {
+        InventoryHandler inventoryHandler1 = handlerCreator.getInventoryHandler();
+        InventoryHandler inventoryHandler2 = handlerCreator.getInventoryHandler();
+        assertSame(inventoryHandler1, inventoryHandler2, "The inventory handler should be the same instance");
     }
 }
