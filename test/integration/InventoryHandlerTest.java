052b007818dc5c3191747be237f595a22c10f145
tahmid <tahmidlasker@gmail.com>
2025-05-02 09:43:40 +0200

    Testing for the inventoryHandler

--- test/integration/InventoryHandlerTest.java
+++ test/integration/InventoryHandlerTest.java
@@ -7,17 +7,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryHandlerTest {
+    private InventoryHandler inventory;
 
     @BeforeEach
     void setUp() {
+        inventory = new InventoryHandler();
     }
 
     @AfterEach
     void tearDown() {
+        inventory = null;
     }
 
     @Test
     void getItemDTO() {
-
+        String itemIDToFind = "abc123";
+        ItemDTO itemToFind = inventory.getItemDTO(itemIDToFind);
+        String expectedOutput = "BigWheel Oatmeal";
+        String actualOutput = itemToFind.getName();
+        assertEquals(expectedOutput, actualOutput, "The item name should be equal");
     }
 }
