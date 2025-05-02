d47569adf28fa23e69d309159d764ec8cb9b36e6
tahmid <tahmidlasker@gmail.com>
2025-05-02 07:40:40 +0200

    Amount test

--- test/model/AmountTest.java
+++ test/model/AmountTest.java
@@ -7,40 +7,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AmountTest {
-    Amount firstAmount;
-    Amount secondAmount;
+    Amount amountWithThree;
+    Amount amountWithFive;
 
     @BeforeEach
     void setUp() {
-        firstAmount = new Amount(1);
-        secondAmount = new Amount(2);
+        amountWithThree = new Amount(3);
+        amountWithFive = new Amount(5);
     }
 
     @AfterEach
     void tearDown() {
-        firstAmount = null;
-        secondAmount = null;
+        amountWithThree = null;
+        amountWithFive = null;
     }
 
     @Test
     void testAdd() {
-        Amount result = firstAmount.add(secondAmount);
-        Amount expected = new Amount(3);
-        assertEquals(expected, result, "The amount should be the same");
+        Amount result = amountWithThree.add(amountWithFive);
+        assertEquals(String.valueOf(8.0), result.toString(), "The sum should be equal.");
     }
 
     @Test
     void testMultiply() {
-        Amount result = firstAmount.multiply(secondAmount);
-        Amount expected = new Amount(2);
-        assertEquals(expected, result, "The amount should be the same");
+        Amount result = amountWithThree.multiply(amountWithFive);
+        assertEquals(String.valueOf(15.0), result.toString(), "The product should be equal.");
     }
 
     @Test
-    void minus() {
+    void testMinus() {
+        Amount result = amountWithFive.minus(amountWithThree);
+        assertEquals(String.valueOf(2.0), result.toString(), "The difference should be equal.");
     }
 
     @Test
     void testToString() {
+        Amount amountWithThreeDecimals = new Amount(3.141);
+        assertEquals("3.14", amountWithThreeDecimals.toString(), "The amount should be equal.");
     }
 }
