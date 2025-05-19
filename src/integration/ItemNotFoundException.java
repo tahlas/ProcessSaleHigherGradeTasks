7ef351676274fd58c1a06aeafad8780d3a5241de
tahmid <tahmidlasker@gmail.com>
2025-05-19 22:36:47 +0200

    removed old code

--- src/integration/ItemNotFoundException.java
+++ src/integration/ItemNotFoundException.java
@@ -4,7 +4,6 @@ package integration;
  * Thrown when an item cannot be found in the inventory (business logic).
  */
 public class ItemNotFoundException  extends Exception{
-    //private final String itemIDThatCannotBeFound;
 
     /**
      * Creates a new instance with a message specifying the item ID that cannot be found.
@@ -12,13 +11,5 @@ public class ItemNotFoundException  extends Exception{
      */
     public ItemNotFoundException(String itemID) {
         super("Item with ID " + itemID + " is not found in the inventory.");
-        //this.itemIDThatCannotBeFound = itemID;
     }
-
-//    /**
-//     * @return The item ID that cannot be added to the sale.
-//     */
-//    public String getItemIDThatCanNotBeFound() {
-//        return itemIDThatCannotBeFound;
-//    }
 }
