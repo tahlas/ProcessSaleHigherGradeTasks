4ae447b4c8735a0d6fd4232f16ae9934f6c8ccc9
tahmid <tahmidlasker@gmail.com>
2025-06-02 00:31:03 +0200

    removed old system.out test

--- test/integration/RegisterTest.java
+++ test/integration/RegisterTest.java
@@ -38,14 +38,14 @@ class RegisterTest {
         System.setOut(originalSysOut);
     }
 
-    @Test
-    void testPresentChangeToGiveToCustomer(){
-        sale.addItem(new ItemDTO("ID", "NAME", new Amount(90), 10, "DESCRIPTION"));
-        sale.payForSale(new CashPayment(new Amount(100)));
-        register.presentChangeToGiveToCustomer(sale);
-        String result = outContent.toString();
-        assertTrue(result.contains("Change to give the customer: 10."),"The change should be printed.");
-    }
+//    @Test
+//    void testPresentChangeToGiveToCustomer(){
+//        sale.addItem(new ItemDTO("ID", "NAME", new Amount(90), 10, "DESCRIPTION"));
+//        sale.payForSale(new CashPayment(new Amount(100)));
+//        register.presentChangeToGiveToCustomer(sale);
+//        String result = outContent.toString();
+//        assertTrue(result.contains("Change to give the customer: 10."),"The change should be printed.");
+//    }
 
     @Disabled
     void testCashInRegisterSystemOut(){
@@ -55,23 +55,23 @@ class RegisterTest {
         assertTrue(result.contains("1100.0"), "The register should print the amount of cash in the register.");
     }
 
-    @Test
-    void testPresentCurrentScannedItem() {
-        ItemDTO item = new ItemDTO("A", "B", new Amount(125), 25, "C" );
-        sale.addItem(item);
-        register.presentCurrentScannedItem(sale, item);
-        String expectedResult =
-                "Add 1 item with item id " + item.getID() + ":\n" +
-                "Item ID: " + item.getID() + "\n" +
-                "Item name: " + item.getName() + "\n" +
-                "Item cost: " + item.getPrice() + "\n" +
-                "VAT: " + item.getVATRatePercentage() + "%\n" +
-                "Item description: " + item.getDescription() + "\n\n" +
-                "Total cost (incl VAT): " + sale.getTotalCostAmount() + " SEK\n" +
-                "Total VAT: " + sale.getTotalVAT();
-        String result = outContent.toString();
-        assertTrue(result.contains(expectedResult), "Wrong register printout.");
-    }
+//    @Test
+//    void testPresentCurrentScannedItem() {
+//        ItemDTO item = new ItemDTO("A", "B", new Amount(125), 25, "C" );
+//        sale.addItem(item);
+//        register.presentCurrentScannedItem(sale, item);
+//        String expectedResult =
+//                "Add 1 item with item id " + item.getID() + ":\n" +
+//                "Item ID: " + item.getID() + "\n" +
+//                "Item name: " + item.getName() + "\n" +
+//                "Item cost: " + item.getPrice() + "\n" +
+//                "VAT: " + item.getVATRatePercentage() + "%\n" +
+//                "Item description: " + item.getDescription() + "\n\n" +
+//                "Total cost (incl VAT): " + sale.getTotalCostAmount() + " SEK\n" +
+//                "Total VAT: " + sale.getTotalVAT();
+//        String result = outContent.toString();
+//        assertTrue(result.contains(expectedResult), "Wrong register printout.");
+//    }
 
     @Test
     void addPaymentToRegister() {
--- test/model/ReceiptTest.java
+++ test/model/ReceiptTest.java
@@ -4,6 +4,7 @@ import integration.ItemDTO;
 import integration.Printer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -28,31 +29,32 @@ class ReceiptTest {
         outContent = null;
         System.setOut(originalSysOut);
     }
-    @Test
-    void testCreateReceiptString() {
-        Sale sale = new Sale();
-        ItemDTO item = new ItemDTO(
-                "abc123",
-                "BigWheel Oatmeal",
-                new Amount(29.9),
-                6,
-                "BigWheel Oatmeal 500 g, whole grain oats, high fiber, gluten free");
-        sale.addItem(item);
-        Amount paidAmount = new Amount(30);
-        sale.payForSale(new CashPayment(paidAmount));
-        LocalDate saleDate = LocalDate.now();
-        Printer printer = new Printer();
-        printer.printReceipt(sale.getReceipt());
-        String result = outContent.toString();
 
-        assertTrue(result.contains("Time of Sale: " + saleDate), "The time of sale should be present in the receipt.");
-        assertTrue(result.contains(item.getName()), "The item name should be present in the receipt.");
-        assertTrue(result.contains("1 x " + item.getPrice()), "The item quantity and price should be present in the receipt.");
-        assertTrue(result.contains(item.getPrice() + " SEK"), "The total item price should be present in the receipt.");
-        assertTrue(result.contains("Total: " + sale.getTotalCostAmount()), "The total price should be present in the receipt.");
-        assertTrue(result.contains("Discounted total: " + sale.getAmountToPay()), "The discounted total should be present in the receipt.");
-        assertTrue(result.contains("VAT: " + sale.getTotalVAT()), "The VAT should be present in the receipt.");
-        assertTrue(result.contains("Cash: " + paidAmount), "The paid amount should be present in the receipt.");
-        assertTrue(result.contains("Change: " + sale.getPayment().getChange()), "The change should be present in the receipt.");
-    }
+//    @Disabled
+//    void testCreateReceiptString() {
+//        Sale sale = new Sale();
+//        ItemDTO item = new ItemDTO(
+//                "abc123",
+//                "BigWheel Oatmeal",
+//                new Amount(29.9),
+//                6,
+//                "BigWheel Oatmeal 500 g, whole grain oats, high fiber, gluten free");
+//        sale.addItem(item);
+//        Amount paidAmount = new Amount(30);
+//        sale.payForSale(new CashPayment(paidAmount));
+//        LocalDate saleDate = LocalDate.now();
+//        Printer printer = new Printer();
+//        printer.printReceipt(sale.getReceipt());
+//        String result = outContent.toString();
+//
+//        assertTrue(result.contains("Time of Sale: " + saleDate), "The time of sale should be present in the receipt.");
+//        assertTrue(result.contains(item.getName()), "The item name should be present in the receipt.");
+//        assertTrue(result.contains("1 x " + item.getPrice()), "The item quantity and price should be present in the receipt.");
+//        assertTrue(result.contains(item.getPrice() + " SEK"), "The total item price should be present in the receipt.");
+//        assertTrue(result.contains("Total: " + sale.getTotalCostAmount()), "The total price should be present in the receipt.");
+//        assertTrue(result.contains("Discounted total: " + sale.getAmountToPay()), "The discounted total should be present in the receipt.");
+//        assertTrue(result.contains("VAT: " + sale.getTotalVAT()), "The VAT should be present in the receipt.");
+//        assertTrue(result.contains("Cash: " + paidAmount), "The paid amount should be present in the receipt.");
+//        assertTrue(result.contains("Change: " + sale.getPayment().getChange()), "The change should be present in the receipt.");
+//    }
 }
